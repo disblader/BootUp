02f9143c4ab9117dbabecedd557392f23e8c7e91
disblader <domas123123@gmail.com>
2014-11-25 19:24:49 +0000

    Test stuff
    Testing some stuff based on the web2py documentation

--- controllers/default.py
+++ controllers/default.py
@@ -17,55 +17,67 @@ def index():
     if you need a simple wiki simply replace the two lines below with:
     return auth.wiki()
     """
-    response.flash = T("Welcome to web2py!")
-    return dict(message=T('Hello World'))
+    # response.flash = T("Welcome to web2py!")
+    if not session.counter:
+        session.counter = 1
+    else:
+        session.counter += 1
+    return dict(message="Potato from my potato", counter = session.counter)
 
+def first():
+    if request.vars.visitor_name:
+        session.visitor_name = request.vars.visitor_name
+        redirect(URL('second'))
+    return dict()
 
-def user():
-    """
-    exposes:
-    http://..../[app]/default/user/login
-    http://..../[app]/default/user/logout
-    http://..../[app]/default/user/register
-    http://..../[app]/default/user/profile
-    http://..../[app]/default/user/retrieve_password
-    http://..../[app]/default/user/change_password
-    http://..../[app]/default/user/manage_users (requires membership in
-    use @auth.requires_login()
-        @auth.requires_membership('group name')
-        @auth.requires_permission('read','table name',record_id)
-    to decorate functions that need access control
-    """
-    return dict(form=auth())
+def second():
+    return dict()
 
-
-@cache.action()
-def download():
-    """
-    allows downloading of uploaded files
-    http://..../[app]/default/download/[filename]
-    """
-    return response.download(request, db)
-
-
-def call():
-    """
-    exposes services. for example:
-    http://..../[app]/default/call/jsonrpc
-    decorate with @services.jsonrpc the functions to expose
-    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
-    """
-    return service()
-
-
-@auth.requires_login() 
-def api():
-    """
-    this is example of API with access control
-    WEB2PY provides Hypermedia API (Collection+JSON) Experimental
-    """
-    from gluon.contrib.hypermedia import Collection
-    rules = {
-        '<tablename>': {'GET':{},'POST':{},'PUT':{},'DELETE':{}},
-        }
-    return Collection(db).process(request,response,rules)
+# def user():
+#     """
+#     exposes:
+#     http://..../[app]/default/user/login
+#     http://..../[app]/default/user/logout
+#     http://..../[app]/default/user/register
+#     http://..../[app]/default/user/profile
+#     http://..../[app]/default/user/retrieve_password
+#     http://..../[app]/default/user/change_password
+#     http://..../[app]/default/user/manage_users (requires membership in
+#     use @auth.requires_login()
+#         @auth.requires_membership('group name')
+#         @auth.requires_permission('read','table name',record_id)
+#     to decorate functions that need access control
+#     """
+#     return dict(form=auth())
+#
+#
+# @cache.action()
+# def download():
+#     """
+#     allows downloading of uploaded files
+#     http://..../[app]/default/download/[filename]
+#     """
+#     return response.download(request, db)
+#
+#
+# def call():
+#     """
+#     exposes services. for example:
+#     http://..../[app]/default/call/jsonrpc
+#     decorate with @services.jsonrpc the functions to expose
+#     supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+#     """
+#     return service()
+#
+#
+# @auth.requires_login()
+# def api():
+#     """
+#     this is example of API with access control
+#     WEB2PY provides Hypermedia API (Collection+JSON) Experimental
+#     """
+#     from gluon.contrib.hypermedia import Collection
+#     rules = {
+#         '<tablename>': {'GET':{},'POST':{},'PUT':{},'DELETE':{}},
+#         }
+#     return Collection(db).process(request,response,rules)
